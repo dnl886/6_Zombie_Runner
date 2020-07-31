2aa9a2bad93164645843df3cff3c85547c66667b
Dan Belepe <danbelepe@gmail.com>
2020-07-31 02:21:39 -0500

    Raycast shooting works now animation next

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float hitPoints =100f;
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage;
+    [SerializeField] ParticleSystem muzleFlash;
+    [SerializeField] GameObject hitFX;
+
+    bool isHit;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessFiring();
+    }
+
+    void PlayMuzzleFlash()
+    {
+        muzleFlash.Play();
+    }
+    void ProcessFiring()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpact(hit);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void CreateHitImpact(RaycastHit hit)
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (hit.collider != null) //This adds lights to objects when u shootat it lol
+
+        GameObject impact = Instantiate(hitFX, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
+    }
+
+}
