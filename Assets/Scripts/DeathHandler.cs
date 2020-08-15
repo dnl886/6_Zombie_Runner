c4e98be082851f4628aef28974e20a269f63009c
dnl886 <61706364+dnl886@users.noreply.github.com>
2020-08-15 02:15:09 -0500

    Gameover System not done yet

---
+++ Assets/Scripts/DeathHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Canvas gameOverCanvas;
+
+    private void Start()
+    {
+        gameOverCanvas.enabled = false;
+    }
+}
