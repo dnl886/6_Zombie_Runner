596c45086029959d2db2bd4384eacbf8a3efbf95
dnl886 <61706364+dnl886@users.noreply.github.com>
2020-08-26 17:12:16 -0500

    Zoom in and out works

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomeOutFov = 60f;
+    [SerializeField] float zoomedInFov = 20f;
+
+    [SerializeField] float zoomOutSensitivity = 2f;
+    [SerializeField] float zoomInSensitivity = .5f;
+
+    RigidbodyFirstPersonController fpsController;
+    [SerializeField] float mouseSensitivity = 5;
+
+    bool zoomeInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(zoomeInToggle == false)
+            {
+                zoomeInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFov;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+            }
+            else
+            {
+                zoomeInToggle = false;
+                fpsCamera.fieldOfView = zoomeOutFov;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+            }
+        }
+    }
+
+    
+}
